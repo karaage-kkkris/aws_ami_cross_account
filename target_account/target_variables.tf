3f865c5d8b0afac65140134415424b19110f497f
Chris Li <chris.li@esentire.com>
2021-08-17 01:40:52 -0400

    Add types to the variables in target account

--- target_account/target_variables.tf
+++ target_account/target_variables.tf
@@ -1,44 +1,54 @@
 # General
 variable "region" {
   description = "Region to deploy into"
+  type        = string
 }
 
 variable "target_permissions_boundary" {
   description = "IAM Permissions Boundary for the target account"
+  type        = string
 }
 
 variable "target_vpc_id" {
   description = "VPC for the target account"
+  type        = string
 }
 
 variable "target_subnet_ids" {
   description = "Subnet IDs for the target account"
+  type        = list(string)
 }
 
 variable "source_account_id" {
   description = "Account ID for the source account"
+  type        = string
 }
 
 variable "source_account_name" {
   description = "Account name for the source account"
+  type        = string
 }
 
 variable "target_account_id" {
   description = "Account ID for the target account"
+  type        = string
 }
 
 variable "target_account_name" {
   description = "Account name for the target account"
+  type        = string
 }
 
 variable "target_component_name" {
   description = "Name of the component"
+  type        = string
   default     = "target-ami-creation"
 }
 
 # EC2
 variable "ec2_instance_type" {
   description = "EC2 instance type"
+  type        = string
   default     = "t3a.small"
 }
 
@@ -71,4 +81,5 @@ variable "ena_support" {
 # Snapshot
 variable "snapshot_id" {
   description = "The Snapshot ID shared by the source account."
+  type        = string
 }
